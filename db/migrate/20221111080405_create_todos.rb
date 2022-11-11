177732c13b01b2a605d25a22c613ef6998fdd0a1
Felix <barosiofelix@gmail.com>
2022-11-11 11:06:20 +0300

    Added values to todo migrations

--- db/migrate/20221111080405_create_todos.rb
+++ db/migrate/20221111080405_create_todos.rb
@@ -1,4 +1,11 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
+    create_table :todos do |t|
+      t.string :text
+      t.string :category
+      t.boolean :done
+
+      t.timestamps null: false
+    end
   end
 end
