23648ce02704e1a434d1413a4c2e589a8bcf379e
Felix <barosiofelix@gmail.com>
2022-11-12 11:56:13 +0300

    Added CORs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,25 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
+  ## Added CORS
+  # set :bind, '0.0.0.0'
+
+  # configure do
+  #     enable :cross_origin
+  # end
+
+  # before do
+  #     response.headers['Access-Control-Allow-Origin'] = 'https://receipe-blog-client.vercel.app'
+  # end
+  
+  # # routes...
+  # options "*" do
+  #     response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  #     response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  #     response.headers["Access-Control-Allow-Origin"] = "*"
+  #     200
+  # end
+  
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
